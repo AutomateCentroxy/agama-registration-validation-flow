6fc2fbf3ecedb70acb261f545b9ee2952b493d10
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-07-03 15:05:13 +0530

    refactor: update UserRegistration.java

--- lib/org/gluu/agama/user/UserRegistration.java
+++ lib/org/gluu/agama/user/UserRegistration.java
@@ -12,7 +12,7 @@ public abstract class UserRegistration {
 
     public abstract boolean passwordPolicyMatch(String userPassword);
 
-    public abstract boolean sendEmailOtp(String email);
+    public abstract String sendEmail(String to, ContextData context);
 
     public abstract boolean validateEmailOtp(String email, String otp);
     
