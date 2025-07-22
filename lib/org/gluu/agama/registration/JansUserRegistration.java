aa8bb7c4f35c7c3c85d3c71daa72126deeb9a4a6
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-07-22 11:44:50 +0530

    refactor: update JansUserRegistration.java

--- lib/org/gluu/agama/registration/JansUserRegistration.java
+++ lib/org/gluu/agama/registration/JansUserRegistration.java
@@ -15,9 +15,6 @@ import org.gluu.agama.smtp.jans.model.ContextData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-
-
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
@@ -35,7 +32,7 @@ import static org.gluu.agama.registration.jans.Attrs.*;
 
 public class JansUserRegistration extends UserRegistration {
 
-    private static final java.util.logging.Logger logger = LoggerFactory.getLogger(JansUserRegistration.class);
+    private static final Logger logger = LoggerFactory.getLogger(JansUserRegistration.class);
 
     private static final String MAIL = "mail";
     private static final String UID = "uid";
@@ -59,19 +56,16 @@ public class JansUserRegistration extends UserRegistration {
 
     private final Map<String, String> emailOtpStore = new HashMap<>();
 
-
     public JansUserRegistration(HashMap config) {
         flowConfig = config;
         logger.debug("Flow config provided is  {}.", config);
     }
 
-    public static synchronized JansUserRegistration getInstance() {
-        if (INSTANCE == null)
-            INSTANCE = new JansUserRegistration();
-        return INSTANCE;
-    }
-
-
+    // public static synchronized JansUserRegistration getInstance() {
+    //     if (INSTANCE == null)
+    //         INSTANCE = new JansUserRegistration();
+    //     return INSTANCE;
+    // }
 
     public boolean passwordPolicyMatch(String userPassword) {
         String regex = '''^(?=.*[!@#$^&*])[A-Za-z0-9!@#$^&*]{6,}$''';
@@ -121,28 +115,28 @@ public class JansUserRegistration extends UserRegistration {
 
     }
 
-
     public String sendOTPCode(String phone) {
-    try {
-        logger.info("Sending OTP Code via SMS to phone: {}", phone);
+        try {
+            logger.info("Sending OTP Code via SMS to phone: {}", phone);
 
-        String otpCode = generateSMSOTpCode(OTP_CODE_LENGTH);
+            String otpCode = generateSMSOTpCode(OTP_CODE_LENGTH);
 
-        logger.info("Generated OTP {} for phone {}", otpCode, phone);
+            logger.info("Generated OTP {} for phone {}", otpCode, phone);
 
-        String message = "Welcome to AgamaLab. This is your OTP Code: " + otpCode;
-        // Store OTP mapped to phone (not username)
-        associateGeneratedCodeToPhone(phone, otpCode);
-        // You can pass `null` or "anonymous" instead of username
-        sendTwilioSms(phone, message);
-        return phone; // Return phone if successful
-    } catch (Exception ex) {
-        logger.error("Failed to send OTP to phone: {}. Error: {}", phone);
-        return null;
+            String message = "Welcome to AgamaLab. This is your OTP Code: " + otpCode;
+            // Store OTP mapped to phone (not username)
+            associateGeneratedCodeToPhone(phone, otpCode);
+            // You can pass `null` or "anonymous" instead of username
+            sendTwilioSms(phone, message);
+            return phone; // Return phone if successful
+        } catch (Exception ex) {
+            logger.error("Failed to send OTP to phone: {}. Error: {}", phone);
+            return null;
+        }
+
     }
 
-    }
-        private String generateSMSOTpCode(int codeLength) {
+    private String generateSMSOTpCode(int codeLength) {
         String numbers = "0123456789";
         SecureRandom random = new SecureRandom();
         char[] otp = new char[codeLength];
@@ -150,32 +144,31 @@ public class JansUserRegistration extends UserRegistration {
             otp[i] = numbers.charAt(random.nextInt(numbers.length()));
         }
         return new String(otp);
-    }   
+    }
 
     private boolean associateGeneratedCodeToPhone(String phone, String code) {
-    try {
-        userCodes.put(phone, code);
-        return true;
-    } catch (Exception e) {
-        logger.error("Error associating OTP code to phone {}. Error: {}", phone, e.getMessage(), e);
-        return false;
+        try {
+            userCodes.put(phone, code);
+            return true;
+        } catch (Exception e) {
+            logger.error("Error associating OTP code to phone {}. Error: {}", phone, e.getMessage(), e);
+            return false;
+        }
     }
-}
-
 
     public boolean validateOTPCode(String phone, String code) {
-    try {
-        logger.info("Validating OTP code {} for phone {}", code, phone);
-        String storedCode = userCodes.getOrDefault(phone, "NULL");
-        if (storedCode.equalsIgnoreCase(code)) {
-            userCodes.remove(phone); // Remove after successful validation
-            return true;
+        try {
+            logger.info("Validating OTP code {} for phone {}", code, phone);
+            String storedCode = userCodes.getOrDefault(phone, "NULL");
+            if (storedCode.equalsIgnoreCase(code)) {
+                userCodes.remove(phone); // Remove after successful validation
+                return true;
+            }
+            return false;
+        } catch (Exception ex) {
+            logger.error("Error validating OTP code {} for phone {}. Error: {}", code, phone, ex.getMessage(), ex);
+            return false;
         }
-        return false;
-    } catch (Exception ex) {
-        logger.error("Error validating OTP code {} for phone {}. Error: {}", code, phone, ex.getMessage(), ex);
-        return false;
-    }
     }
 
     public String addNewUser(Map<String, String> profile, Map<String, String> passwordInput) throws Exception {
@@ -268,7 +261,7 @@ public class JansUserRegistration extends UserRegistration {
 
     }
 
-        private boolean sendTwilioSms(String phone, String message) {
+    private boolean sendTwilioSms(String phone, String message) {
         try {
 
             PhoneNumber FROM_NUMBER = new com.twilio.type.PhoneNumber(flowConfig.get("FROM_NUMBER"));
@@ -286,13 +279,13 @@ public class JansUserRegistration extends UserRegistration {
             Message.creator(TO_NUMBER, FROM_NUMBER, message).create();
 
             logger.info("OTP code has been successfully send to {} on phone number {} .", phone);
-            
+
             return true;
         } catch (Exception exception) {
-            logger.error("Error sending OTP code to user {} on pone number {} : error {} .", phone, exception.getMessage(), exception);
+            logger.error("Error sending OTP code to user {} on pone number {} : error {} .", phone,
+                    exception.getMessage(), exception);
             return false;
         }
     }
 
 }
-
